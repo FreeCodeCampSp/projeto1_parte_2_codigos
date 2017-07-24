fc107c97d1c79be40305df58803b34f02fe0c619
drFabio <blackjackdevel@gmail.com>
2017-07-24 22:29:19 +0200

    Add compilacao em pug

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Olá mundo!')
+// Pega o pacote para lidar com arquivos do node (fs = file system, sistema de arquivos)
+var fs = require('fs')
+// Pega o pacote instalado pug
+var pug = require('pug')
+//Retorna uma função que sabe transformar nosso template em HTML
+var compileFunction = pug.compileFile('template.pug')
+// Escreve em um arquivo chamado  resultado.html
+fs.writeFileSync('resultado.html', compileFunction())
