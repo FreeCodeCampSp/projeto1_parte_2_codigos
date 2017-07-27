07a9f443ff8569c1509e604d1cec32b58870b5a0
drFabio <blackjackdevel@gmail.com>
2017-07-27 21:49:10 +0200

    Separado dados

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ var fs = require('fs')
 // Pega o pacote instalado pug
 var pug = require('pug')
 //Retorna uma função que sabe transformar nosso template em HTML
+
+var allEvents = [
+  {
+    name: 'Programação defensiva - Como fazer seus robôs não te matarem',
+    date: '01/02/2900'
+    },
+  {
+    name: 'Arrumando seus braços robóticos com PHP',
+    date: '04/02/2900'
+    },
+  {
+    name: 'Desenvolvimento de jogos ... Mortais',
+    date: '07/05/2900'
+    },
+  {
+    name: 'Hackeando a matrix para negociar aumentos',
+    date: '01/06/2900'
+    },
+  {
+    name: 'Programando robôs gigantes utilizando NodeJS',
+    date: '09/06/2900'
+    }
+]
 var compileFunction = pug.compileFile('template.pug', {pretty: true})
 // Escreve em um arquivo chamado  resultado.html
-fs.writeFileSync('resultado.html', compileFunction())
+fs.writeFileSync('resultado.html', compileFunction({allEvents: allEvents}))
